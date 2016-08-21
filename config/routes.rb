2b8b481133ae72445667b7addf1940d996ec60c1
Kamil <kamil.milewski@hotmail.com>
2016-08-21 22:57:47 +0200

    Create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :categories do
+  	resources :topics do
+  	  resources :posts
+  	end
+  end
+
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
