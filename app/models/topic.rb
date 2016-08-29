46faa75dbfd35921a603458763670681208db31f
Kamil <kamil.milewski@hotmail.com>
2016-08-29 23:02:44 +0200

    Update models and db schema

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ApplicationRecord
 	has_many :posts
 	belongs_to :user
-	belongs_to :category
+	belongs_to :category, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820213406) do
+ActiveRecord::Schema.define(version: 20160829205041) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "topics", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_topics_on_category_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -36,6 +27,17 @@ ActiveRecord::Schema.define(version: 20160820213406) do
     t.datetime "updated_at", null: false
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["category_id"], name: "index_topics_on_category_id"
+    t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
