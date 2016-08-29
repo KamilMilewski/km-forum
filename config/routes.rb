ecd00a3214a6bd1c2907e3440b14c939f1210899
Kamil <kamil.milewski@hotmail.com>
2016-08-29 20:27:36 +0200

    Rearange router to avoid too nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,25 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
 
+  # To avoid nesting resources too much, instead of following:
+  #
+  # resources :categories do
+  # 	resources :topics do
+  # 	  resources :posts
+  # 	end
+  # end
+  #
+  # We are using:
 
   resources :categories do
-  	resources :topics do
-  	  resources :posts
-  	end
+    resources :topics, name_prefix: 'category_'
   end
 
+  resources :topics do
+    resources :posts, name_prefix: 'topic_'
+  end
+
+  resources :posts
   resources :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
