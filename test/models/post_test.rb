0574f6d0d25a40b05dc9983d42e3567b69f0e27f
Kamil <kamil.milewski@hotmail.com>
2016-08-31 20:56:30 +0200

    Create post model tests and improve model validation

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:one)
+  end
+
+  test "valid post should be... valid" do
+    assert @post.valid?,
+          "Example post from fixture file should pass validation."
+  end
+
+  test "content can't be blank" do
+    @post.content = ' '
+    assert_not @post.valid?,
+              "Post model validaton shouldn't allow content attr. to be blank."
+  end
+
 end
