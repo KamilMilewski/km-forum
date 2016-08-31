8a32187d2641b05e8257a3f6d155248fc67445fd
Kamil <kamil.milewski@hotmail.com>
2016-08-31 21:04:30 +0200

    added bcrypt gem and has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
 	has_many :topics
 	has_many :posts
+
+	#This function requires bcrypt gem installed and password_digest column to be
+	#present.It adds following funcionality:
+	# -Save securely hashed password digest to the database.
+	# -It adds up a pair of virtual attributes: password and password_confirmation.
+	# -It adds validation if those two are present and match.
+	# -It provides authenticate method that returns user if password is correct and
+	#  false otherwise.
+	has_secure_password
+
 end
