00e4319198228cc070c7358ffef1105830f84eb0
Kamil <kamil.milewski@hotmail.com>
2016-08-31 20:44:42 +0200

    Create topic model tests and implement corresponding model validation

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,7 @@ class Topic < ApplicationRecord
 	has_many :posts, dependent: :destroy
 	belongs_to :user
 	belongs_to :category
+
+	validates :title, presence: true, length: {maximum: 255}
+	validates :content, presence: true
 end
--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @topic = topics(:one)
+  end
+
+  test "valid topic should be... valid" do
+    assert @topic.valid?,
+          "Example topic from fixture file should pass validation."
+  end
+
+  test "title can't be blank" do
+    @topic.title = ' '
+    assert_not @topic.valid?,
+              "Topic model validaton shouldn't allow title attr. to be blank."
+  end
+
+  test "title cant't be too long" do
+    @topic.title = 'a' * 256
+    assert_not @topic.valid?,
+              "Topic model validation shouldn't allow title attr. to be longer
+                than 255 chars"
+  end
+
+  test "content can't be blank" do
+    @topic.content = "  "
+    assert_not @topic.valid?,
+              "Topic model validaton shouldn't allow content attr. to be blank."
+  end
 end
