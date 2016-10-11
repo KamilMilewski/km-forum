645c6f396949b64525793f074c8a6259370f07ab
Kamil Milewski <kamil.milewski@hotmail.com>
2016-10-11 22:13:58 +0200

    Create account edit page with gravatar edit link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def gravatar_url user
     email_digest = Digest::MD5::hexdigest(user.email.downcase)
-    gravater_url = "https://secure.gravatar.com/avatar/#{email_digest}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{email_digest}"
   end
 end
