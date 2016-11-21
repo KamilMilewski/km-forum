f6f208d289c7f421dadcff66f1aec43269fa2f58
Kamil Milewski <kamil.milewski@hotmail.com>
2016-11-21 21:40:57 +0100

    Refactor user create tests

--- test/integration/user_create_test.rb
+++ test/integration/user_create_test.rb
@@ -7,66 +7,101 @@ class UserCreateTest < ActionDispatch::IntegrationTest
     ActionMailer::Base.deliveries.clear
   end
 
-  test 'invalid user creation (signup)' do
+  test 'should allow user enter signup page' do
     get new_user_path
-    assert_response :success
     assert_template 'users/new'
-    # Test if create user(signup) with invalid data will fail.
-    assert_no_difference 'User.count' do
-      post users_path, params: {
-        user: {
-          name:                   'valid test name',
-          email:                  'valid@user.email',
-          permissions:            'villainous_hacker',
-          password:               'validpassword',
-          password_confirmation:  ''
-        }
-      }
-    end
-    assert_template 'users/new'
-    assert_flash_notices danger: { count: 1 }
-    # Check if there are fields with erroes.
-    assert_select 'div.field_with_errors'
+    assert_flash_notices
   end
 
-  test 'valid user creation (signup) with account activation' do
-    get new_user_path
-    assert_response :success
-    assert_template 'users/new'
-    # Test if user create vith valid data vill succeed.
+  test 'should allow user create new account' do
     assert_difference 'User.count', 1 do
       post users_path, params: {
         user: {
-          name:                   'valid test name',
-          email:                  'valid@user.email',
-          permissions:            'admin',
-          password:               'validpassword',
-          password_confirmation:  'validpassword'
+          name: 'Valid user name',
+          email: 'valid@user.email',
+          permissions: 'user',
+          password: 'uuuuuu',
+          password_confirmation: 'uuuuuu'
         }
       }
     end
-    # Assure that activation email has been sent.
-    assert_equal 1, ActionMailer::Base.deliveries.size
-    # Access instance variable @user in corresponding controller action.
+
+    # :FIXME assigns should not be used in but I don't have better idea.
+    # Newly created user.
     user = assigns(:user)
+
+    # Right after filling the form user is send to main page.
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_template 'categories/index'
+    # The notice about account activatin pops up.
+    assert_flash_notices info: { count: 1 }
+
+    # Account activation.
+    # Assure that activatin email has been sent.
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    # Assert new user isn't activated.
     assert_not user.activated?
-    # Try to log in before account activatin and assure it fails.
+    # Try to log in user before activation and assure it fails. He is being
+    # redirected to main page with message about account being not activated.
     log_in_as(user)
     assert_not logged_in?
-    # Try to activate account with INVALID token and assure it fails.
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_flash_notices warning: { count: 1 }
+    # Try to activate account with invalid token and assure it fails.
     get edit_account_activation_path('invalid token', email: user.email)
     assert_not logged_in?
-    # Try to activate account with VALID token but INVALID email and assure it
-    # fails.
+    # Try to activate account with valid token/invalid email and assure it fails
     get edit_account_activation_path(user.activation_token, email: 'invalid')
     assert_not logged_in?
-    # Finally try to activate account with VALID token and email and assure it
-    # will succeed.
+    # Assert activatin with valid token and email will succeed.
     get edit_account_activation_path(user.activation_token, email: user.email)
     assert user.reload.activated?
+    # Right after activating accont user is logged in and redirected to his
+    # profile page.
     follow_redirect!
     assert_template 'users/show'
-    assert_flash_notices success: { count: 1 }
     assert logged_in?
+    assert_flash_notices success: { count: 1 }
+  end
+
+  test 'sould NOT allow user create new account with invalid data' do
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: '',
+          email: 'invalid_user.email',
+          permissions: 'admin',
+          password: 'some password',
+          password_confirmation: 'obviously different password'
+        }
+      }
+    end
+
+    # Assure no activation email has been sent.
+    assert_equal 0, ActionMailer::Base.deliveries.size
+
+    assert_template 'users/new'
+    # Check if there are form fileds with errors.
+    assert_select 'div.field_with_errors'
+    assert_flash_notices danger: { count: 1 }
+  end
+
+  test 'should NOT allow user create account with admin permissions' do
+    # Someone adds 'permissions: admin' to other parameters during user creation
+    post users_path, params: {
+      user: {
+        name: 'Valid user name',
+        email: 'valid@user.email',
+        permissions: 'admin',
+        password: 'aaaaaa',
+        password_confirmation: 'aaaaaa'
+      }
+    }
+
+    # :FIXME assigns should not be used in but I don't have better idea.
+    # assert that in the end, new user still has only 'user' level permissions.
+    assert assigns(:user).user?
   end
 end
