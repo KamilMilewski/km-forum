2ce4c2d5242286685aee77d06d62b7f72b8defec
Kamil Milewski <kamil.milewski@hotmail.com>
2016-12-11 01:07:37 +0100

    Implement posts_count in Category model - first draft

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,13 @@ class Category < ApplicationRecord
   has_many :topics, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
+
+  # Calculates post count in this category.
+  # Raw sql is used to leverage all load to db and max performance.
+  def posts_count
+    topic_ids = "SELECT id FROM topics
+                 WHERE category_id = #{id}"
+    sql_query = "SELECT COUNT (*) FROM posts WHERE topic_id IN (#{topic_ids})"
+    records_array = ActiveRecord::Base.connection.execute(sql_query)
+  end
 end
